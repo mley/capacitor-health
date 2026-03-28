b1ea9c144f81019722e496885f369b41d79aff1f
Brandon Hudson <bhud07@gmail.com>
2026-03-28 16:42:46 -0400

    Fix

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorHealth",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorHealth",
